f382fd784bed2fba4b05ebb8f9bbfa5d89162788
wskp90 <reimar.zeh@fau.de>
2023-10-04 17:29:17 +0200

    Covariates added

--- script/models_mulder.R
+++ script/models_mulder.R
@@ -666,7 +666,7 @@ Ly5 ~~ vy*Ly5
 x1 + x2 + x3 + x4 + x5 ~ mx*1
 y1 + y2 + y3 + y4 + y5 ~ my*1'
 
-## RI-CLPModell 5 Wellen a time-invariant predictor of the observed variables ####
+## RI-CLPModell 5 Wellen time-invariant predictor for OVs ####
 DF$riclpm5CV <- '
 # Create between components (random intercepts)
 RIx =~ 1*x1 + 1*x2 + 1*x3 + 1*x4 + 1*x5
@@ -721,6 +721,59 @@ Ly4 ~~ Ly4
 Lx5 ~~ Lx5
 Ly5 ~~ Ly5 '
 
+## RI-CLPModell 6 Wellen time-invariant predictor for RI ####
+DF$riclpm5RCV <- '
+# Create between components (random intercepts)
+RIx =~ 1*x1 + 1*x2 + 1*x3 + 1*x4 + 1*x5
+RIy =~ 1*y1 + 1*y2 + 1*y3 + 1*y4 + 1*y5
+  
+# Estimate variance and covariance of random intercepts
+RIx ~~ RIx
+RIy ~~ RIy
+RIx ~~ RIy
+  
+# Regression of random intercepts on z1
+RIx + RIy ~ z1 # Constrained over time. 
+  
+# Create within-person centered variables
+Lx1 =~ 1*x1
+Lx2 =~ 1*x2
+Lx3 =~ 1*x3 
+Lx4 =~ 1*x4
+Lx5 =~ 1*x5
+Ly1 =~ 1*y1
+Ly2 =~ 1*y2
+Ly3 =~ 1*y3
+Ly4 =~ 1*y4
+Ly5 =~ 1*y5
+  
+# Estimate lagged effects between within-person centered variables
+Lx2 + Ly2 ~ Lx1 + Ly1
+Lx3 + Ly3 ~ Lx2 + Ly2
+Lx4 + Ly4 ~ Lx3 + Ly3
+Lx5 + Ly5 ~ Lx4 + Ly4
+  
+# Estimate covariance between within-person centered variables at first wave
+Lx1 ~~ Ly1 # Covariance
+  
+# Estimate covariances between residuals of within-person centered variables 
+# (i.e., innovations)
+Lx2 ~~ Ly2
+Lx3 ~~ Ly3
+Lx4 ~~ Ly4
+Lx5 ~~ Ly5
+  
+# Estimate (residual) variance of within-person centered variables
+Lx1 ~~ Lx1 # Variances
+Ly1 ~~ Ly1 
+Lx2 ~~ Lx2 # Residual variances
+Ly2 ~~ Ly2 
+Lx3 ~~ Lx3 
+Ly3 ~~ Ly3 
+Lx4 ~~ Lx4 
+Ly4 ~~ Ly4 
+Lx5 ~~ Lx5
+Ly5 ~~ Ly5'
 
 ## RI-CLPModell 6 Wellen unconstrained  Mulder ####
 DF$riclpm6 <- '
@@ -846,7 +899,8 @@ Ly6 ~~ vy*Ly6
 # Constrain grand means over time
 x1 + x2 + x3 + x4 + x5 + x6 ~ mx*1
 y1 + y2 + y3 + y4 + y5 + y6 ~ my*1'
-## RI-CLPModell 6 Wellen a time-invariant predictor of the observed variables ####
+
+## RI-CLPModell 6 Wellen time-invariant predictor for OVs ####
 DF$riclpm6CV <- '
 # Create between components (random intercepts)
 RIx =~ 1*x1 + 1*x2 + 1*x3 + 1*x4 + 1*x5 + 1*x6
@@ -870,6 +924,65 @@ Ly6 =~ 1*y6
 x1 + x2 + x3 + x4 + x5 ~ s1*z1 # Constrained over time. 
 y1 + y2 + y3 + y4 + y5 ~ s2*z1 # Constrained over time. 
   
+# Estimate lagged effects between within-person centered variables
+Lx6 + Ly6 ~ Lx5 + Ly5
+Lx5 + Ly5 ~ Lx4 + Ly4
+Lx4 + Ly4 ~ Lx3 + Ly3
+Lx3 + Ly3 ~ Lx2 + Ly2
+Lx2 + Ly2 ~ Lx1 + Ly1
+
+# Estimate covariance between within-person centered variables at first wave
+Lx1 ~~ Ly1 # Covariance
+
+# Estimate covariances between residuals of within-person centered variables 
+# (i.e., innovations)
+Lx2 ~~ Ly2
+Lx3 ~~ Ly3
+Lx4 ~~ Ly4
+Lx5 ~~ Ly5
+Lx6 ~~ Ly6
+  
+  # Estimate variance and covariance of random intercepts
+RIx ~~ RIx
+RIy ~~ RIy
+RIx ~~ RIy
+
+# Estimate (residual) variance of within-person centered variables
+Lx1 ~~ Lx1 # Variances
+Ly1 ~~ Ly1 
+Lx2 ~~ Lx2 # Residual variances
+Ly2 ~~ Ly2 
+Lx3 ~~ Lx3 
+Ly3 ~~ Ly3 
+Lx4 ~~ Lx4 
+Ly4 ~~ Ly4 
+Lx5 ~~ Lx5
+Ly5 ~~ Ly5
+Lx6 ~~ Lx6
+Ly6 ~~ Ly6'
+
+## RI-CLPModell 6 Wellen time-invariant predictor for RI ####
+DF$riclpmR6RCV <- '
+# Create between components (random intercepts)
+RIx =~ 1*x1 + 1*x2 + 1*x3 + 1*x4 + 1*x5 + 1*x6
+RIy =~ 1*y1 + 1*y2 + 1*y3 + 1*y4 + 1*y5 + 1*y6
+
+# Create within-person centered variables
+Lx1 =~ 1*x1
+Lx2 =~ 1*x2
+Lx3 =~ 1*x3 
+Lx4 =~ 1*x4
+Lx5 =~ 1*x5
+Lx6 =~ 1*x6
+Ly1 =~ 1*y1
+Ly2 =~ 1*y2
+Ly3 =~ 1*y3
+Ly4 =~ 1*y4
+Ly5 =~ 1*y5
+Ly6 =~ 1*y6
+  
+RIx + RIy ~ z1 # Constrained over time. 
+
 # Estimate lagged effects between within-person centered variables
 Lx6 + Ly6 ~ Lx5 + Ly5
 Lx5 + Ly5 ~ Lx4 + Ly4
